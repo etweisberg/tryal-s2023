a95149cd28cd3e3b01de8ee2860084bb7da1f5f1
christopherwun <ckywun123@gmail.com>
2023-02-22 17:02:05 -0500

    icon test for windows (successful)

---
+++ mobile/App.tsx
@@ -0,0 +1,23 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import Icon from "react-native-vector-icons/FontAwesome";
+
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.tsx to start working on your app!</Text>
+      <Icon name="rocket" size={30} color="900"/>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
