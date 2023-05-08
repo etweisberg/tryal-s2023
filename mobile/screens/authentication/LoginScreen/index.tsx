d460198c233e4084f1627baf9916a6c17d9c5297
christopherwun <ckywun123@gmail.com>
2023-05-08 12:48:38 -0400

    ran server + mobile together

--- mobile/screens/authentication/LoginScreen/index.tsx
+++ mobile/screens/authentication/LoginScreen/index.tsx
@@ -27,7 +27,7 @@ export default function LoginScreen({ navigation }: { navigation: any}) {
 
   const handleLogin = async () => {
     try {
-      const response = await fetch('http://localhost:3000/auth/login', {
+      const response = await fetch('http://localhost:4000/auth/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
--- server/src/config/createExpressApp.ts
+++ server/src/config/createExpressApp.ts
@@ -34,7 +34,9 @@ const createExpressApp = (sessionStore: MongoStore): express.Express => {
     }),
   );
   // Gives express the ability accept origins outside its own to accept requests from
-  app.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
+  // app.use(cors());
+  // app.use(cors({ credentials: true, origin: 'http://localhost:3000' }));
+  app.use(cors({ credentials: true, origin: 'http://192.168.4.36:19000' }));
   // Gives express the ability to parse client cookies and add them to req.cookies
   app.use(cookieParser(process.env.COOKIE_SECRET));
 
