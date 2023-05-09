1690074d040103f0180f49399d9eb6a342417e9a
christopherwun <ckywun123@gmail.com>
2023-05-09 17:03:36 -0400

    basic components

--- mobile/screens/participant/SavedScreen/index.tsx
+++ mobile/screens/participant/SavedScreen/index.tsx
@@ -1,10 +1,24 @@
-import { View, Text, SafeAreaView } from 'react-native'
+import { View, Text, SafeAreaView, ScrollView } from 'react-native'
 import React from 'react'
+import StudyList, { DataItem } from '../../../components/StudyList'
+import Header from '../../../components/Header';
+
+const DATA: DataItem[] = [
+  { id: '1', title: 'Card 1', description: 'This is the first card' },
+  { id: '2', title: 'Card 2', description: 'This is the second card' },
+  { id: '3', title: 'Card 3', description: 'This is the third card' },
+];
 
 export default function SavedScreen() {
   return (
-    <SafeAreaView>
-      <Text>SavedScreen</Text>
-    </SafeAreaView>
+    <View>
+      <ScrollView showsVerticalScrollIndicator={false} style={{flex: 1, width: '100%'}}>
+
+        <Header title='Saved'/>
+
+        <StudyList data={DATA} />        
+      </ScrollView>
+
+    </View>
   )
 }
